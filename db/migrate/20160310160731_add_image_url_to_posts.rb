d5534873071432269135b2a736f5641dd0d8a280
Jorge Padilla <jorgep4dill4@gmail.com>
2016-03-13 00:58:13 -0500

    image field added

---
+++ db/migrate/20160310160731_add_image_url_to_posts.rb
@@ -0,0 +1,5 @@
+class AddImageUrlToPosts < ActiveRecord::Migration
+  def change
+    add_column :posts, :url, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160307052130) do
+ActiveRecord::Schema.define(version: 20160310160731) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 20160307052130) do
     t.text     "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "url"
   end
 
 end
